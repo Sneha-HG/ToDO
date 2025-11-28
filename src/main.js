b84c85f7cba99c314538bb88b1b92b5eddcce907
Sneha-HG <Sneha.hg@virtuele.us>
2025-11-28 13:13:07 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { createApp } from 'vue'
+// importing router instance
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App) // vue app created
+app.use(router) // router is getting activated 
+app.mount('#app') //mounting the app to dom
